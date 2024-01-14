9d43e9e3db96ec448ca906815549e86b64112223
Dedsec <raitra007@gmail.com>
2024-01-14 20:51:00 +0300

    Initial Commit

---
+++ src/main/java/com/culture/API/Controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.culture.API.Controller;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+	private static final String template = "Hello, %s!";
+
+	@GetMapping("/greeting")
+	public ResponseEntity<String> greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new ResponseEntity<>(String.format(template, name), HttpStatus.OK);
+	}
+}
